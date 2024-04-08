4af1dd9cf958f69a267f3ae0864bbe7b0eed8059
iankristoper <iankristoper26@gmail.com>
2024-04-08 15:46:47 +0800

    UPDATE: interface class features

---
+++ src/main/java/com/ian/employee_management_system_role_based_permissions/EmployeeRole.java
@@ -0,0 +1,14 @@
+package com.ian.employee_management_system_role_based_permissions;
+
+/**
+ *
+ * @author Ian Tagano
+ */
+public interface EmployeeRole 
+{
+    public void viewEmployeeDetails();
+    public void editEmployeeDetails();
+    public void deleteEmployee();
+}
+
+
